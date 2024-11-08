a0321af14417834cf310e8a23ab0fb6d0d8239cb
Akhila640 <akhilasiddi00@gmail.com>
2024-11-08 11:00:51 +0530

    Update day1.js

--- day1.js
+++ day1.js
@@ -2,30 +2,30 @@
 
 
 // 1)WAP to print numbers from -1 to -num(input) without using >= operator.
-// num=prompt("enter a number: ")
-// for(i=-1;!(i<=num);i--){
-//     console.log(i)
-// }
+num=prompt("enter a number: ")
+for(i=-1;!(i<=num);i--){
+    console.log(i)
+}
 
 // 2)WAP to print numbers from -num(input) to -1 without using <= operator.
-// // num=prompt("enter a number")
-// for(i=num;!(i>=0);i++){
-//     console.log(i)
-// }
+// num=prompt("enter a number")
+for(i=num;!(i>=0);i++){
+    console.log(i)
+}
 
 
 
 // 3) WAP to print sum of even digits in a number
 // input: Enter a number: 1287
 // output: Sum is: 10
-// num=prompt("enter a number: ")
-// sum=0
-// if (num>0){
-//     for(i=1;i<num.length;i++)
-//         if(num[i] % 2 == 0)
-//             sum=sum+Number(num[i])
-// }
-// console.log(sum)
+num=prompt("enter a number: ")
+sum=0
+if (num>0){
+    for(i=1;i<num.length;i++)
+        if(num[i] % 2 == 0)
+            sum=sum+Number(num[i])
+}
+console.log(sum)
 
 // 4) WAP to print sum of odd digits in a number
 // input: Enter a number: 1811
